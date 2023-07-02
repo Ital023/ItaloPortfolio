9a311dd6c3efa9be7124960aff25819e245323c1
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-07-02 02:00:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var botaoMenu = document.querySelector(".cabecalho__menu");
+var menu = document.querySelector(".menu-lateral");
+
+botaoMenu.addEventListener("click",()=>{
+    menu.classList.toggle("menu-lateral__ativo");
+})
