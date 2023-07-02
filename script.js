b2c80028a20671b9ef2e431c97949b105bd58cec
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-07-02 13:49:57 -0300

    ajustes na home

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 var botaoMenu = document.querySelector(".cabecalho__menu");
 var menu = document.querySelector(".menu-lateral");
-var principal = document.querySelector(".principal");
 
 botaoMenu.addEventListener("click",()=>{
     menu.classList.toggle("menu-lateral__ativo");
-    principal.classList.toggle("principal--ativo");
-    principal.classList.toggle("principal");
 })
 
