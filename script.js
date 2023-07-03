1282f490a1be2f82de96fa0eca4ef85007e86195
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-07-03 16:50:04 -0300

    Adicionando cards

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var botaoMenu = document.querySelector(".cabecalho__menu");
 var menu = document.querySelector(".menu-lateral");
+var sidebar = document.querySelector("#sidebar");
+var toggle = document.querySelector("#toggle");
+
+document.onclick = function(e){
+    if(!botaoMenu.contains(e.target) && !menu.contains(e.target)){
+        menu.classList.remove("menu-lateral__ativo");
+    }
+}
 
 botaoMenu.addEventListener("click",()=>{
     menu.classList.toggle("menu-lateral__ativo");
-})
-
+}) 
